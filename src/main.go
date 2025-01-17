14ad58425b25143ff019cd51f8dce95059e03447
James Dwyer <jcdwyer@jcd-dev.net>
2025-01-16 18:39:23 -0800

    getting rudimentary stats

--- src/main.go
+++ src/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bike/models"
 	"encoding/json"
 	"fmt"
+
 	"os"
 )
 
@@ -16,12 +18,45 @@ func main() {
 		panic(err)
 	}
 	defer file.Close()
-	var result map[string]interface{}
+	//var result map[string]interface{}
+	var result models.RIDE_DATA
 	decoder := json.NewDecoder(file)
 
 	err = decoder.Decode(&result)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print(result["RIDE"])
+	fmt.Println(result)
+	//fmt.Println(result.Ride.Tags)
+	var max float32
+	var powerRanges [100]uint64
+	ftp := float32(250.0)
+	overCount := uint64(0)
+	underCount := uint64(0)
+	zeroCount := uint64(0)
+	for _, sample := range result.Ride.Samples {
+
+		if sample.Watts > max {
+			max = sample.Watts
+		}
+		if sample.Watts <= 1.0 {
+			zeroCount++
+		} else if sample.Watts >= ftp {
+			overCount++
+		} else {
+			underCount++
+		}
+		idx := int16(sample.Watts / 25)
+		powerRanges[idx] = powerRanges[idx] + 1
+
+	}
+	maxIdx := uint16(max / 25)
+	var i uint16
+	for i = 0; i < maxIdx; i++ {
+		fmt.Printf("range %d %d : count %d\n", i*25, i*25+25, powerRanges[i])
+
+	}
+
+	fmt.Printf("max : %f\n", max)
+	fmt.Printf("Zero %d Over %d Under %d\n", zeroCount, overCount, underCount)
 }
---
+++ src/models/ride.go
@@ -0,0 +1,56 @@
+package models
+
+type RIDE_DATA struct {
+	Ride RIDE `json:"RIDE"`
+}
+
+type RIDE struct {
+	StartTime  string          `json:"STARTTIME"`
+	RecIntSecs int             `json:"RECINTSECS"`
+	DeviceType string          `json:"DEVICETYPE"`
+	Identifier string          `json:"IDENTIFIER"`
+	Tags       RIDE_TAGS       `json:"TAGS"`
+	Intervals  []RIDE_INTERVAL `json:"INTERVALS"`
+	Samples    []RIDE_SAMPLE   `json:"SAMPLES"`
+}
+
+type RIDE_SAMPLE struct {
+	Secs  uint64  `json:"SECS"`
+	Km    float32 `json:"KM"`
+	Watts float32 `json:"WATTS"`
+	Cad   float32 `json:"CAD"`
+	Kph   float32 `json:"KPH"`
+	Hr    float32 `json:"HR"`
+	Alt   float32 `json:"ALT"`
+	Lat   float32 `json:"LAT"`
+	Long  float32 `json:"LON"`
+	Slope float32 `json:"SLOPE"`
+	Temp  float32 `json:"TEMP"`
+	Lppb  float32 `json:"LPPB"`
+	Rppb  float32 `json:"RPPB"`
+	Lppe  float32 `json:"LPPE"`
+	Rppe  float32 `json:"RPPE"`
+	Lpppb float32 `json:"LPPPB"`
+	Rpppb float32 `json:"RPPPB"`
+	Lpppe float32 `json:"LPPPE"`
+	Rpppe float32 `json:"RPPPE"`
+}
+
+type RIDE_INTERVAL struct {
+	Name  string `json:"NAME"`
+	Start int    `json:"START"`
+	Stop  int    `json:"STOP"`
+	Color string `json:"COLOR"`
+	Ptest string `json:"PTEST"`
+}
+
+type RIDE_TAGS struct {
+	Athlete                 string `json:"Athlete"`
+	AnaerobicTrainingEffect string `json:"Anaerobic Training Effect"`
+
+	AerobicTrainingEffect string `json:"Aerobic Training Effect"`
+	ChangeHistory         string `json:"Change History"`
+	Data                  string `json:"Data"`
+	Device                string `json:"Device"`
+	DeviceInfo            string `json:"Device Info"`
+}
