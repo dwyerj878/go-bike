5c22da13d7b8c4f1a6441be838e4c2ac32472d01
jcdwyer <jcdwyer@jcd-dev.net>
2025-03-03 22:15:29 -0600

    initial chart impl

--- src/main.go
+++ src/main.go
@@ -81,7 +81,7 @@ func httpserver(w http.ResponseWriter, _ *http.Request) {
 	}
 
 	// Put data into instance
-	line.SetXAxis([]string{"0", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}).
+	line.SetXAxis([]string{"0", "100", "200", "300", "400", "500", "600", "700", "800", "900", "1000"}).
 		AddSeries("Power", power).
 		AddSeries("Speed", speed).
 		SetSeriesOptions(
